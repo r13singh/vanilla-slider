02af6ce4a0f9cd906459bc43db08062feda1d09d
Rahul Singh <rahulsingh2928@gmail.com>
2021-01-25 23:03:29 +0530

    added percent data-attribute to slider html element

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ class Slider {
     this.sliderElem = document.querySelector('.slider');
     this.sliderThumbElem = document.querySelector('.slider__thumb');
     this.sliderFillElem = document.querySelector('.slider__fill');
+    this.sliderContainerElem = document.querySelector('.slider-container');
 
-    this.sliderX = 100;
     this.onPointerMove = this.onPointerMove.bind(this);
     this.onPointerDown = this.onPointerDown.bind(this);
     this.onPointerUp = this.onPointerUp.bind(this);
 
-    this.initPercent = 40;
+    this.percent = this.sliderContainerElem.dataset.percent;
     this.sliderWidth = parseInt(window.getComputedStyle(this.sliderElem).width, 10);
     
-    this.sliderFillElem.style.transform = `translateX(-${100 - this.initPercent}%)`;
-    this.sliderThumbElem.style.left = (this.sliderWidth * (this.initPercent / 100) - Slider.SLIDER_THUMB_WIDTH) + 'px';
+    this.sliderFillElem.style.transform = `translateX(-${100 - this.percent}%)`;
+    this.sliderThumbElem.style.left = (this.sliderWidth * (this.percent / 100) - Slider.SLIDER_THUMB_WIDTH) + 'px';
     
     this.addEventListeners();
   }
@@ -28,7 +28,15 @@ class Slider {
 
   onPointerMove(e) {
     let percentFilled = Math.round(e.offsetX / this.sliderWidth * 100);
+    
     percentFilled = percentFilled > 100 ? 100 : percentFilled;
+    if (percentFilled < 0) {
+      percentFilled = 0;
+    } else if (percentFilled > 100) {
+      percentFilled = 100;
+    }
+    this.sliderContainerElem.dataset.percent = percentFilled;
+    
     this.sliderFillElem.style.transform = `translateX(-${100 - percentFilled}%)`;
     
     let sliderX = e.offsetX - Slider.SLIDER_THUMB_WIDTH;
